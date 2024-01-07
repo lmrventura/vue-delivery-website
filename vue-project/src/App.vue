7599492901034187cc793458bbfd5f7bb5969c67
luizmrventura <luizmrv@outlook.com>
2024-01-06 23:40:43 -0300

    almost-done

---
+++ vue-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="div">
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view/>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make your Burger"
+    }
+  },
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style>
+  * {
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+</style>
